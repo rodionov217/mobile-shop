48e0b67adccb5ea2fc19ce87259b4a39cae5335b
rodionov217 <rodionova217@gmail.com>
2019-08-19 15:36:25 +0300

    build

---
+++ build/precache-manifest.dfd7fcd6ac53b7172dfb0029a26acd06.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3389c0f2111de893d52a535ea1643f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9aa7236a241edd2208a",
+    "url": "/static/css/main.06666ef8.chunk.css"
+  },
+  {
+    "revision": "323601aa7f4e584b57ac",
+    "url": "/static/js/2.55bb2d0c.chunk.js"
+  },
+  {
+    "revision": "d9aa7236a241edd2208a",
+    "url": "/static/js/main.3ba89474.chunk.js"
+  },
+  {
+    "revision": "9f844312ce377d85e72b",
+    "url": "/static/js/runtime~main.6530d08c.js"
+  },
+  {
+    "revision": "7b9d3eed026bc7d75cd1ff073475e1e0",
+    "url": "/static/media/books.7b9d3eed.png"
+  },
+  {
+    "revision": "eca6781d1b3930929d462cf7a32f9793",
+    "url": "/static/media/pen.eca6781d.png"
+  },
+  {
+    "revision": "de05ae920b04157f5f03c582f04e95b1",
+    "url": "/static/media/product-img.de05ae92.png"
+  },
+  {
+    "revision": "4c2b1bbbaa7bcce7b1b3e685ee015de2",
+    "url": "/static/media/start-img.4c2b1bbb.png"
+  },
+  {
+    "revision": "b58c28716434e61336665016498c6b6d",
+    "url": "/static/media/top1.b58c2871.png"
+  },
+  {
+    "revision": "91c7be059614064f584d9e553f64016b",
+    "url": "/static/media/top2.91c7be05.png"
+  },
+  {
+    "revision": "4c67a863471f21e9d1cec5fccd54a5ca",
+    "url": "/static/media/top3.4c67a863.png"
+  },
+  {
+    "revision": "3a152723fd894bf1c3cb654fb5af3bda",
+    "url": "/static/media/top4.3a152723.png"
+  }
+]);
